ffa584eeafe61ab43b75580d9361611fec0e3819
Shravan R Hegde <163821861+shravan-cc@users.noreply.github.com>
2024-07-02 17:34:10 +0530

    Implemented Zod Schema Definitions and Tests for Book Entities

--- article-management/models/book.model.ts
+++ article-management/models/book.model.ts
@@ -1,14 +1,38 @@
-export interface IBookBase {
-  title: string;
-  author: string;
-  publisher: string;
-  genre: string;
-  isbnNo: string;
-  numOfPages: number;
-  totalNumOfCopies: number;
-}
+import { z } from 'zod';
 
-export interface IBook extends IBookBase {
-  id: number;
-  availableNumOfCopies: number;
-}
+// export interface IBookBase {
+//   title: string;
+//   author: string;
+//   publisher: string;
+//   genre: string;
+//   isbnNo: string;
+//   numOfPages: number;
+//   totalNumOfCopies: number;
+// }
+
+// export interface IBook extends IBookBase {
+//   id: number;
+//   availableNumOfCopies: number;
+// }
+
+export const bookBaseSchema = z.object({
+  title: z.string(),
+  author: z.string(),
+  publisher: z.string(),
+  genre: z.string(),
+  isbnNo: z.string(),
+  numOfPages: z.number().int().positive(),
+  totalNumOfCopies: z.number().int().positive(),
+});
+
+
+export const bookSchema = bookBaseSchema.extend({
+  id: z.number().int().positive(),
+  availableNumOfCopies: z.number().int().nonnegative(),
+});
+
+export type IBookBase = z.infer<typeof bookBaseSchema>;
+export type IBook = z.infer<typeof bookSchema>
+
+export const bookBaseSchemaArray = z.array(bookBaseSchema);
+export const bookSchemaArray = z.array(bookSchema);
