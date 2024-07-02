5b1b7e9a9dd6ac310095c3fc4abfe4ccf7837283
“vignesh” <vignesh.h@codecraft.co.in>
2024-07-02 22:41:42 +0530

    Implemented editbook function and previous menu

--- article-management/book.interactor.ts
+++ article-management/book.interactor.ts
@@ -1,4 +1,3 @@
-import { I } from 'vitest/dist/reporters-yx5ZTtEV';
 import { readChar, readLine } from '../core/input.utils';
 import { IInteractor } from '../core/interactor';
 import { BookRepository } from './book.repository';
@@ -10,13 +9,15 @@ const menu = new Menu([
   { key: '2', label: 'Edit Book' },
   { key: '3', label: 'Search Book' },
   { key: '4', label: 'Delete Book' },
-  { key: '4', label: '<Previous Menu>' },
+  { key: '5', label: 'Display Book' },
+  { key: '6', label: '<Previous Menu>' },
 ]);
 
 export class BookInteractor implements IInteractor {
   private repo = new BookRepository();
   async showMenu(): Promise<void> {
-    while (true) {
+    let loop: boolean = true;
+    while (loop) {
       const op = await readChar(menu.serialize());
       const menuItem = menu.getItem(op);
       if (menuItem) {
@@ -25,18 +26,23 @@ export class BookInteractor implements IInteractor {
       switch (op.toLowerCase()) {
         case '1':
           await addBook(this.repo);
+          console.table(this.repo.list({ limit: 100, offset: 0 }).items);
           break;
         case '2':
-          // TODO: edit book flow
+          await editBook(this.repo);
+          console.table(this.repo.list({ limit: 100, offset: 0 }).items);
           break;
         case '3':
-          console.table(this.repo.list({ limit: 1000, offset: 0 }).items);
+          console.table(this.repo.list({ limit: 100, offset: 0 }).items);
           break;
         case '4':
-          // TODO: update book
+          // TODO: delete book
           break;
         case '5':
-          // TODO: previous menu
+          // TODO: display list
+          break;
+        case '6':
+          loop = false;
           break;
         default:
           console.log('Invalid Choice!!');
@@ -45,24 +51,53 @@ export class BookInteractor implements IInteractor {
   }
 }
 
-async function getBookInput() {
-  const title = await readLine('please enter title: ');
-  const author = await readLine('Please enter author: ');
-  const publisher = await readLine('Please enter publisher: ');
-  const genre = await readLine('Please enter genre: ');
-  const isbnNo = await readLine('Please enter ISBN Number: ');
-  const numOfPages = await readLine('Please enter total num of pages: ');
-  const totalNumOfCopies = await readLine(
-    'Please enter the total num of copies: '
+async function getBookInput(existingData?: IBookBase): Promise<IBookBase> {
+  const title = await readLine(
+    `Please enter title${
+      existingData?.title ? ` (${existingData.title})` : ''
+    }: `
   );
+  const author = await readLine(
+    `Please enter author${
+      existingData?.author ? ` (${existingData.author})` : ''
+    }: `
+  );
+  const publisher = await readLine(
+    `Please enter publisher${
+      existingData?.publisher ? ` (${existingData.publisher})` : ''
+    }: `
+  );
+  const genre = await readLine(
+    `Please enter genre${
+      existingData?.genre ? ` (${existingData.genre})` : ''
+    }: `
+  );
+  const isbnNo = await readLine(
+    `Please enter ISBN Number${
+      existingData?.isbnNo ? ` (${existingData.isbnNo})` : ''
+    }: `
+  );
+  const numOfPages = await readLine(
+    `Please enter total num of pages${
+      existingData?.numOfPages ? ` (${existingData.numOfPages})` : ''
+    }: `
+  );
+  const totalNumOfCopies = await readLine(
+    `Please enter the total num of copies${
+      existingData?.totalNumOfCopies
+        ? ` (${existingData.totalNumOfCopies})`
+        : ''
+    }: `
+  );
+
   return {
-    title: title,
-    author: author,
-    publisher: publisher,
-    genre: genre,
-    isbnNo: isbnNo,
-    numOfPages: +numOfPages,
-    totalNumOfCopies: +totalNumOfCopies,
+    title: title || existingData?.title || '',
+    author: author || existingData?.author || '',
+    publisher: publisher || existingData?.publisher || '',
+    genre: genre || existingData?.genre || '',
+    isbnNo: isbnNo || existingData?.isbnNo || '',
+    numOfPages: +numOfPages || existingData?.numOfPages || 0,
+    totalNumOfCopies: +totalNumOfCopies || existingData?.totalNumOfCopies || 0,
   };
 }
 
@@ -71,4 +106,27 @@ async function addBook(repo: BookRepository) {
   const createdBook = repo.create(book);
   console.log('Book added successfully!\n');
   console.table(createdBook);
+}
+
+async function editBook(repo: BookRepository) {
+  const id = +(await readLine('Enter the ID of the book to edit: '));
+  const existingBook = repo.getById(id);
+
+  if (!existingBook) {
+    console.log('Book not found!');
+    return;
+  }
+
+  console.log('Existing book details:');
+  console.table(existingBook);
+
+  const updatedData = await getBookInput(existingBook);
+  const updatedBook = repo.update(id, updatedData);
+
+  if (updatedBook) {
+    console.log('Book updated successfully!\n');
+    console.table(updatedBook);
+  } else {
+    console.log('Failed to update book. Please try again.');
+  }
 }
