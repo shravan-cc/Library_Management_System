7e12b57f766e27d198cebc8fc428cbec8e2cc1b7
prethesh <preethesh.devadiga@codecraft.co.in>
2024-07-02 21:28:30 +0530

    Implemented member schema, repository, and interactor with validation tests

---
+++ member-management/member.repository.ts
@@ -0,0 +1,28 @@
+import { IMember, IMemberBase } from "./models/member.model";
+import { IRepository } from '../core/repository';
+import { IPageRequest, IPagedResponse } from '../core/pagination.response';
+
+const members: IMember[] = [];
+
+export class MemberRepository implements IRepository<IMemberBase, IMember> {
+    create(data: IMemberBase): IMember{
+        const member: IMember = {
+          ...data,
+            memberId: members.length + 1,
+        };
+        members.push(member)
+        return member;
+    }
+    update(id: number, data: { firstName: string; lastName: string; phone: number; membershipDate: Date; booksIssued: string[]; }): { firstName: string; lastName: string; phone: number; membershipDate: Date; booksIssued: string[]; memberId: number; } | null {
+        throw new Error("Method not implemented.");
+    }
+    delete(id: number): { firstName: string; lastName: string; phone: number; membershipDate: Date; booksIssued: string[]; memberId: number; } | null {
+        throw new Error("Method not implemented.");
+    }
+    getById(id: number): { firstName: string; lastName: string; phone: number; membershipDate: Date; booksIssued: string[]; memberId: number; } | null {
+        throw new Error("Method not implemented.");
+    }
+    list(params: IPageRequest): IPagedResponse<{ firstName: string; lastName: string; phone: number; membershipDate: Date; booksIssued: string[]; memberId: number; }> {
+        throw new Error("Method not implemented.");
+    } 
+}
