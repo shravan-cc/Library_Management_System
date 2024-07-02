a6c4c1115880b6c06d26c66aeddce3dd71c3ef27
“vignesh” <vignesh.h@codecraft.co.in>
2024-07-02 17:34:23 +0530

    Fixed: Resolve loop issue in BookInteractor and LibraryInteractor function

--- article-management/book.interactor.ts
+++ article-management/book.interactor.ts
@@ -1,30 +1,46 @@
+import { I } from 'vitest/dist/reporters-yx5ZTtEV';
 import { readChar, readLine } from '../core/input.utils';
 import { IInteractor } from '../core/interactor';
 import { BookRepository } from './book.repository';
 import { IBookBase } from './models/book.model';
-const menu = `
-    1. Add Book
-    2. Edit Book
-    3. Search Book
-    4. <Previous Menu>
-    `;
+import { Menu } from '../core/menu';
+
+const menu = new Menu([
+  { key: '1', label: 'Add Book' },
+  { key: '2', label: 'Edit Book' },
+  { key: '3', label: 'Search Book' },
+  { key: '4', label: 'Delete Book' },
+  { key: '4', label: '<Previous Menu>' },
+]);
+
 export class BookInteractor implements IInteractor {
   private repo = new BookRepository();
   async showMenu(): Promise<void> {
-    const op = await readChar(menu);
-    switch (op.toLowerCase()) {
-      case '1':
-        addBook(this.repo);
-        break;
-      case '2':
-        // TODO: add book flow
-        break;
-      case '3':
-        console.table(this.repo.list({ limit: 1000, offset: 0 }).items);
-        break;
-      case '4':
-        // TODO: add book flow
-        break;
+    while (true) {
+      const op = await readChar(menu.serialize());
+      const menuItem = menu.getItem(op);
+      if (menuItem) {
+        console.log(`Choice: ${menuItem.key}.\t${menuItem.label}`);
+      }
+      switch (op.toLowerCase()) {
+        case '1':
+          await addBook(this.repo);
+          break;
+        case '2':
+          // TODO: edit book flow
+          break;
+        case '3':
+          console.table(this.repo.list({ limit: 1000, offset: 0 }).items);
+          break;
+        case '4':
+          // TODO: update book
+          break;
+        case '5':
+          // TODO: previous menu
+          break;
+        default:
+          console.log('Invalid Choice!!');
+      }
     }
   }
 }
@@ -53,6 +69,6 @@ async function getBookInput() {
 async function addBook(repo: BookRepository) {
   const book: IBookBase = await getBookInput();
   const createdBook = repo.create(book);
-  console.log('book added successfully!\n');
+  console.log('Book added successfully!\n');
   console.table(createdBook);
 }
