f512d5017d918576993027b1351dd9a278c3b039
Shravan <shravan.hegde@codecraft.co.in>
2024-07-03 17:00:05 +0530

    Added tests for all the methods of member

--- member-management/member.repository.test.ts
+++ member-management/member.repository.test.ts
@@ -1,57 +1,84 @@
-import { describe, beforeEach } from 'vitest';
-
+import { describe, beforeEach, test, expect } from 'vitest';
 import { MemberRepository } from './member.repository';
 import { IMember, IMemberBase } from './models/member.model';
-import exp from 'constants';
 
-describe('Tests for member class methods', () => {
-  let members: IMember[];
-  let memberRepository: any;
+describe('Tests for MemberRepository class methods', () => {
+  let memberRepository: MemberRepository;
+  let data: IMemberBase;
   beforeEach(() => {
-    members = [
-      {
-        address: 'Mangalore',
-        firstName: 'Shravan',
-        lastName: 'Hegde',
-        memberId: 1,
-        phone: 8792225251,
-      },
-      {
-        address: 'Mangalore',
-        firstName: 'Preethesh',
-        lastName: 'Devadiga',
-        memberId: 2,
-        phone: 9632483331,
-      },
-      {
-        address: 'Mangalore',
-        firstName: 'Vignesh',
-        lastName: 'Devadiga',
-        memberId: 3,
-        phone: 9632453331,
-      },
-    ];
     memberRepository = new MemberRepository();
-  });
-  test('Tests for creating member', () => {
-    const data: IMemberBase = members[0];
-    expect(memberRepository.create(data)).toEqual({
+    data = {
       address: 'Mangalore',
       firstName: 'Shravan',
       lastName: 'Hegde',
-      memberId: 1,
       phone: 8792225251,
+    };
+  });
+
+  test('Tests for creating member', () => {
+    const createdMember = memberRepository.create(data);
+    expect(createdMember).toEqual({
+      ...data,
+      memberId: 1,
     });
+
     expect(memberRepository.create(data)).toBeDefined();
   });
 
-  test('Tests for deleting members ', () => {
-    expect(memberRepository.delete(members[0].memberId)).toEqual({
+  test('Tests for deleting member', () => {
+    const createdMember = memberRepository.create(data);
+    expect(memberRepository.delete(createdMember.memberId)).toEqual(
+      createdMember
+    );
+  });
+
+  test('Tests for updating member', () => {
+    const createdMember = memberRepository.create(data);
+
+    const updatedData: IMemberBase = {
+      address: 'Udupi',
+      firstName: 'Shravan Kumar',
+      lastName: 'Hegde',
+      phone: 8792225251,
+    };
+
+    const updatedMember = memberRepository.update(
+      createdMember.memberId,
+      updatedData
+    );
+    expect(updatedMember).toEqual({
+      ...updatedData,
+      memberId: createdMember.memberId,
+    });
+  });
+
+  test('Tests for getting member by ID', () => {
+    const createdMember = memberRepository.create(data);
+
+    const retrievedMember = memberRepository.getById(createdMember.memberId);
+    expect(retrievedMember).toEqual(createdMember);
+  });
+
+  test('Tests for listing members', () => {
+    const data1: IMemberBase = {
       address: 'Mangalore',
       firstName: 'Shravan',
       lastName: 'Hegde',
-      memberId: 1,
       phone: 8792225251,
-    });
+    };
+
+    const data2: IMemberBase = {
+      address: 'Mangalore',
+      firstName: 'Preethesh',
+      lastName: 'Devadiga',
+      phone: 9632483331,
+    };
+
+    const createdMember1 = memberRepository.create(data1);
+    const createdMember2 = memberRepository.create(data2);
+
+    const result = memberRepository.list({ offset: 0, limit: 10, search: '' });
+
+    expect(result.pagination.total).toBe(6);
   });
 });
